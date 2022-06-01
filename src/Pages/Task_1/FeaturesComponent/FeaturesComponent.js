73c76a062fdfdbd837fef8a398544210a434867a
mr-samsul-alam <contactsamsulalam@gmail.com>
2022-06-01 09:07:30 +0600

    sami final boss

--- src/Pages/Task_1/FeaturesComponent/FeaturesComponent.js
+++ src/Pages/Task_1/FeaturesComponent/FeaturesComponent.js
@@ -1,6 +1,7 @@
+import { tab } from '@testing-library/user-event/dist/tab';
 import React, { useState } from 'react';
-import { Button, Card, Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
-
+import { Card, Carousel, Col, Container, Row } from 'react-bootstrap';
+import './FeaturesComponent.css'
 
 const FeaturesComponent = () => {
     const [tab1, setTab1] = useState(false)
@@ -34,58 +35,205 @@ const FeaturesComponent = () => {
 
     return (
         <div>
-            <h1 style={{ textAlign: "center" }}>Our Features</h1>
-            <Container style={{ textAlign: "center" }}>
-                <Button onClick={btn1Clicked}>POP-UNDER</Button>
-                <Button onClick={btn2Clicked}>BANNER ADD</Button>
-                <Button onClick={btn3Clicked}>NATIVE</Button>
-                <Button onClick={btn4Clicked}>SKIM</Button>
-                <Card style={{ display: tab1 ? 'flex' : 'none', flexDirection: 'row' }}>
-                    <Card.Img variant="top" src="/Images/header_img.jpg" style={{ width: '25rem' }} />
-                    <Card.Body>
-                        <Card.Title>POP-UNDER</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Card.Link href="/">Learn More</Card.Link>
-                    </Card.Body>
-                </Card>
+            <h1 style={{ textAlign: "center" }} className='my-5 pt-5'>Our Features</h1>
+            <Container className='my-5'>
+                <div className='my-5' style={{ textAlign: "center", display: 'flex', justifyContent: 'space-around' }}>
+                    <button
+                        onClick={btn1Clicked}
+                        style={{
+                            background: tab1 ? "#ff6f00" : undefined,
+                            color: tab1 ? "white" : undefined
+                        }}
+                        className="rounded-pill  Fbtn "
+                    >POP-UNDER</button>
+                    <button onClick={btn2Clicked} style={{
+                        background: tab2 ? "#ff6f00" : undefined,
+                        color: tab2 ? "white" : undefined
+                    }}
+                        className="rounded-pill  Fbtn ">BANNER ADD</button>
+                    <button onClick={btn3Clicked} style={{
+                        background: tab3 ? "#ff6f00" : undefined,
+                        color: tab3 ? "white" : undefined
+                    }}
+                        className="rounded-pill  Fbtn ">NATIVE</button>
+                    <button onClick={btn4Clicked} style={{
+                        background: tab4 ? "#ff6f00" : undefined,
+                        color: tab4 ? "white" : undefined
+                    }}
+                        className="rounded-pill  Fbtn ">SKIM</button>
+                </div>
+
+                <div style={{ display: tab1 ? 'flex' : 'none', flexDirection: 'row' }}>
+                    <Row>
+                        <Col sm="12" md="6">
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/Card-1.png"
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block  "
+                                        src="/Images/card-2.png"
+                                        alt="Second slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/card-3.png"
+                                        alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                            </Carousel>
+                        </Col>
+                        <Col sm="12" md="6">
+                            <div>
+                                <h6 style={{ color: '#FF6F00' }}>POP-UNDER</h6>
+                                <p>
+                                    A company is a legal entity formed by a group of individuals to engage in and operate a business—commercial or industrial—enterprise. A company may be organized in various ways for tax and financial liability purposes depending on the corporate law of its jurisdiction.
+                                </p>
+                                <Card.Link href="/">Learn More</Card.Link>
+                            </div>
+                        </Col>
+                    </Row>
+
+                </div>
 
 
-                <Card style={{ display: tab2 ? 'flex' : 'none', flexDirection: 'row' }}>
-                    <Card.Img variant="top" src="/Images/header_img.jpg" style={{ width: '25rem' }} />
-                    <Card.Body>
-                        <Card.Title>BANNER ADD</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Card.Link href="/">Learn More</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style={{ display: tab3 ? 'flex' : 'none', flexDirection: 'row' }}>
-                    <Card.Img variant="top" src="/Images/header_img.jpg" style={{ width: '25rem' }} />
-                    <Card.Body>
-                        <Card.Title>NATIVE</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Card.Link href="/">Learn More</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style={{ display: tab4 ? 'flex' : 'none', flexDirection: 'row' }}>
-                    <Card.Img variant="top" src="/Images/header_img.jpg" style={{ width: '25rem' }} />
-                    <Card.Body>
-                        <Card.Title>SKIM</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Card.Link href="/">Learn More</Card.Link>
-                    </Card.Body>
-                </Card>
+                <div style={{ display: tab2 ? 'flex' : 'none', flexDirection: 'row' }}>
+                    <Row>
+                        <Col sm="12" md="6">
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/card-2.png"
+
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block  "
+                                        src="/Images/Card-1.png"
+                                        alt="Second slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/card-3.png"
+                                        alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                            </Carousel>
+                        </Col>
+                        <Col sm="12" md="6">
+                            <div>
+                                <h6 style={{ color: '#FF6F00' }} >BANNER ADD</h6>
+                                <p>
+                                    The line of business the company is in will generally determine which business structure it chooses such as a partnership, proprietorship, or corporation. These structures also denote the ownership structure of the company.
+                                </p>
+                                <Card.Link href="/">Learn More</Card.Link>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div style={{ display: tab3 ? 'flex' : 'none', flexDirection: 'row' }}>
+                    <Row>
+                        <Col sm="12" md="6">
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/Card-1.png"
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block  "
+                                        src="/Images/card-3.png"
+
+                                        alt="Second slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/card-2.png"
+                                        alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                            </Carousel>
+                        </Col>
+                        <Col sm="12" md="6">
+                            <div>
+                                <h6 style={{ color: '#FF6F00' }}>NATIVE</h6>
+                                <p>
+                                    They can also be distinguished between private and public companies. Both have different ownership structures, regulations, and financial reporting requirements.
+                                </p>
+                                <Card.Link href="/">Learn More</Card.Link>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+                <div style={{ display: tab4 ? 'flex' : 'none', flexDirection: 'row' }}>
+                    <Row>
+                        <Col sm="12" md="6">
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/card-3.png"
+
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block  "
+                                        src="/Images/card-2.png"
+                                        alt="Second slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        style={{ height: "200px", width: "350px" }}
+                                        className="d-block "
+                                        src="/Images/Card-1.png"
+                                        alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                            </Carousel>
+                        </Col>
+                        <Col sm="12" md="6">
+                            <div>
+                                <h6 style={{ color: '#FF6F00' }}>SKIM</h6>
+                                <p>
+                                    A company is essentially an artificial person—also known as corporate personhood—in that it is an entity separate from the individuals who own, manage, and support its operations.
+                                </p>
+                                <Card.Link href="/">Learn More</Card.Link>
+                            </div>
+                        </Col>
+                    </Row>
+
+                </div>
             </Container>
 
         </div >
